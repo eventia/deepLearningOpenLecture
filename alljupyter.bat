3850e747907b2e4ddc7ba949256cc6b4ddaab60f
eventia <eventia@gmail.com>
2018-08-22 08:09:18 +0900

    Add:Lect11-AffineLayer

--- alljupyter.bat
+++ alljupyter.bat
@@ -1,4 +1,5 @@
 REM echo To Quit just Type "deactivate"
 REM echo cmd /k .\tf36\Scripts\activate | jupyter notebook
-cmd /k .\tf36\Scripts\activate 
-jupyter notebook
+cd D:\OpenDEV\MachineLearning\deepLearningOpenLecture
+cmd /k "cd D:\OpenDEV\MachineLearning\deepLearningOpenLecture & .\tf36\Scripts\activate & jupyter notebook"
+REM jupyter notebook
